93821edbdbd1a6cf0e132b363373ddd2a91bbaa0
Mayra <mayra@iMac-de-Mayra.local>
2021-12-15 12:54:26 -0800

    Updates

---
+++ precache-manifest.16ce990adf73beed06522bdeb71313df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7320eac055ecd9e18b055909c1ba31d1",
+    "url": "/token-sale/index.html"
+  },
+  {
+    "revision": "4b7397945b148077ac1d",
+    "url": "/token-sale/static/css/2.1f023efc.chunk.css"
+  },
+  {
+    "revision": "d449cb30112c22234404",
+    "url": "/token-sale/static/css/main.ef0e7bc8.chunk.css"
+  },
+  {
+    "revision": "4b7397945b148077ac1d",
+    "url": "/token-sale/static/js/2.cbbc4032.chunk.js"
+  },
+  {
+    "revision": "9e1ca0ddf3e0bcb36d6ee4df4c0f5cf4",
+    "url": "/token-sale/static/js/2.cbbc4032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d449cb30112c22234404",
+    "url": "/token-sale/static/js/main.a59c5026.chunk.js"
+  },
+  {
+    "revision": "7b72f2c498ffe57ff088",
+    "url": "/token-sale/static/js/runtime-main.6b96655a.js"
+  },
+  {
+    "revision": "582113f5e7ffc6c720d829af9212f763",
+    "url": "/token-sale/static/media/logo.582113f5.png"
+  }
+]);
